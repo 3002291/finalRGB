e9f041030010243769b7d6ae5426f24321e825af
최진웅 <plz200>
2022-11-02 17:19:05 +0900

    [2022-11-02][P20221019070] 배출시설등록 신규메뉴 개발

---
+++ src/main/java/com/douzone/comet/service/eh/ehsevr/dao/Ehsevr00200_DAO.java
@@ -0,0 +1,52 @@
+package com.douzone.comet.service.eh.ehsevr.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.comet.jdbc.mybatis.DzMybatisSupport;
+import com.douzone.comet.service.eh.ehsevr.models.Ehsevr00200;
+/** 
+  * @description :
+  * @Since   : 
+  * @Author  :
+  * @History :
+  */
+@Repository
+public class Ehsevr00200_DAO  {
+
+	private DzMybatisSupport mybatisSupport;
+
+	@Autowired
+	public Ehsevr00200_DAO(DzMybatisSupport mybatisSupport) {
+		this.mybatisSupport = mybatisSupport;
+	}
+
+	
+    public List<Ehsevr00200> list(String name,	HashMap<String, Object> parameters) throws Exception {
+    	return this.mybatisSupport.selectList(this.getClass().getName() + ".selectList", parameters);
+    }
+
+	public void deleteEhsevr00200(Ehsevr00200 Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.delete(this.getClass().getName() + ".deleteEhsevr00200", Ehsevr00200_temp);
+	}
+	public void deleteEhsevr00200(List<Ehsevr00200> Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.updateBatch(this.getClass().getName() + ".deleteEhsevr00200", Ehsevr00200_temp);
+	}
+	
+	public void updateEhsevr00200(Ehsevr00200 Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.update(this.getClass().getName() + ".updateEhsevr00200", Ehsevr00200_temp);
+	}
+	public void updateEhsevr00200(List<Ehsevr00200> Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.updateBatch(this.getClass().getName() + ".updateEhsevr00200", Ehsevr00200_temp);
+	}
+
+	public void insertEhsevr00200(Ehsevr00200 Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.insert(this.getClass().getName() + ".insertEhsevr00200", Ehsevr00200_temp);
+	}
+	public void insertEhsevr00200(List<Ehsevr00200> Ehsevr00200_temp) throws Exception {
+		this.mybatisSupport.insertBatch(this.getClass().getName() + ".insertEhsevr00200", Ehsevr00200_temp);
+	}
+}
