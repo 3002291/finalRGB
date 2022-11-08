94fabad25c368d36cd49514061bbd52db2c4a13e
최진웅 <plz200>
2022-11-08 14:16:39 +0900

    [2022-11-08][P20221020004] 용수원급원등록 신규메뉴 개발

---
+++ src/main/java/com/douzone/comet/service/eh/ehsevr/models/Ehsevr00300Drop1.java
@@ -0,0 +1,41 @@
+package com.douzone.comet.service.eh.ehsevr.models;
+ 
+import com.douzone.gpd.jdbc.mybatis.model.DzAbstractModel;
+import com.douzone.gpd.restful.annotation.DzModel;
+import com.douzone.gpd.restful.annotation.DzModelField;
+import com.google.gson.annotations.SerializedName;
+ 
+@DzModel(name = "Ehsevr00300Drop1", desc = "Ehsevr00300Drop1", tableName = "")
+public class Ehsevr00300Drop1 extends DzAbstractModel {
+
+	@SerializedName("MNG_DTL_CD")
+	@DzModelField(name="mng_dtl_cd", desc="관리상세코드", colName="MNG_DTL_CD", colSize="20", isKey=false)
+	private String mng_dtl_cd;
+	  
+	@SerializedName("MNG_DTL_NM")
+	@DzModelField(name="mng_dtl_nm", desc="관리상세명", colName="MNG_DTL_NM", colSize="100", isKey=false)
+	private String mng_dtl_nm;
+
+	public String getMng_dtl_cd() {
+		return mng_dtl_cd;
+	}
+
+	public void setMng_dtl_cd(String mng_dtl_cd) {
+		this.mng_dtl_cd = mng_dtl_cd;
+	}
+ 
+	public String getMng_dtl_nm() {
+		return mng_dtl_nm;
+	}
+
+	public void setMng_dtl_nm(String mng_dtl_nm) {
+		this.mng_dtl_nm = mng_dtl_nm;
+	}
+	
+    @Override
+    public String toString() {
+      return
+        "mng_dtl_cd: ["+ mng_dtl_cd +"]\n"+
+        "mng_dtl_nm: ["+ mng_dtl_nm +"]";
+    }
+}
